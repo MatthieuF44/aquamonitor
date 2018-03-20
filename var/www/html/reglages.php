8a34cfd08fff3bfb716a890c52ad7622e815ceca
Matthieu Fleury <matthieu.fleury44@gmail.com>
2018-03-20 21:55:18 +0100

    - Ajout des sliders de réglages pours les seuils - Ajout du bouton switch pour le mode automatique

--- var/www/html/reglages.php
+++ var/www/html/reglages.php
@@ -61,7 +61,86 @@ else{
 					}
 					?>
 					<div class="ui-grid-a">
-						Test
+						<form>
+							<div class="ui-corner-all custom-corners">
+								<div class="ui-bar ui-bar-a">
+									<h3>Réglage des seuils d'alerte pour les différents capteurs</h3>
+								</div>
+								<div class="ui-body ui-body-a">
+									<div data-role="collapsibleset" data-theme="a" data-content-theme="a">
+										<?php
+										$sqltemp1 = "SELECT * FROM `sensor`";
+										$resulttemp1 = mysqli_query($connection, $sqltemp1) or die(mysqli_error($connection));
+										if(mysqli_num_rows($resulttemp1) != 0) {
+											while($rowtemp1 =mysqli_fetch_assoc($resulttemp1)){
+												//Traduction des noms de type
+												if ($rowtemp1["type"]=="Temp"){
+													$type="Température";
+												}
+												elseif ($rowtemp1["type"]=="Level"){
+													$type="Niveau";
+												}
+												else {
+													$type=$rowtemp1["type"];
+												}
+												//Réglage des mini, maxi en fonction des types de capteur
+												if ($rowtemp1["type"]=="Temp"){
+													$slider_min=10;
+													$slider_max=30;
+												}
+												elseif ($rowtemp1["type"]=="Level"){
+													$slider_min=0;
+													$slider_max=125;
+												}
+												elseif ($rowtemp1["type"]=="Ph"){
+													$slider_min=0;
+													$slider_max=14;
+												}
+												else {
+													$slider_min=0;
+													$slider_max=100;
+												}
+												?>
+												<div data-role="collapsible" data-collapsed-icon="gear">
+													<h3><?php echo $type." - ".$rowtemp1["name"]; ?></h3>
+													<div class="ui-field-contain">
+														<div data-role="rangeslider" data-mini="true">
+															<label for="seuil-<?php echo $rowtemp1["id"]; ?>-min">1er niveau (seuil bas):</label>
+															<input type="range" name="seuil-<?php echo $rowtemp1["id"]; ?>-min" id="seuil-<?php echo $rowtemp1["id"]; ?>-min" min="<?php echo $slider_min; ?>" max="<?php echo $slider_max; ?>" value="<?php echo $rowtemp1["min"]; ?>" step=".1">
+															<label for="seuil-<?php echo $rowtemp1["id"]; ?>-max">1er niveau (seuil bas):</label>
+															<input type="range" name="seuil-<?php echo $rowtemp1["id"]; ?>-max" id="seuil-<?php echo $rowtemp1["id"]; ?>-max" min="<?php echo $slider_min; ?>" max="<?php echo $slider_max; ?>" value="<?php echo $rowtemp1["max"]; ?>" step=".1">
+														</div>
+													</div>
+													<div class="ui-field-contain">
+														<div data-role="rangeslider" data-mini="true">
+															<label for="seuil-<?php echo $rowtemp1["id"]; ?>-min0">2ème niveau (seuil critique):</label>
+															<input type="range" name="seuil-<?php echo $rowtemp1["id"]; ?>-min0" id="seuil-<?php echo $rowtemp1["id"]; ?>-min0" min="<?php echo $slider_min; ?>" max="<?php echo $slider_max; ?>" value="<?php echo $rowtemp1["min0"]; ?>" step=".1">
+															<label for="seuil-<?php echo $rowtemp1["id"]; ?>-max0">2ème niveau (seuil critique):</label>
+															<input type="range" name="seuil-<?php echo $rowtemp1["id"]; ?>-max0" id="seuil-<?php echo $rowtemp1["id"]; ?>-max0" min="<?php echo $slider_min; ?>" max="<?php echo $slider_max; ?>" value="<?php echo $rowtemp1["max0"]; ?>" step=".1">
+														</div>
+													</div>
+												</div>
+												<?php
+											}
+										}
+										?>
+									</div>
+								</div>
+							</div>
+							<p></p>
+							<div class="ui-bar ui-bar-a">
+								<h3>Gestion du mode automatique)</h3>
+							</div>
+							<div class="ui-body ui-body-a">
+								<div class="ui-field-contain">
+									<label for="slider-flip-m">Contrôle du co2</label>
+									<select name="slider-flip-m" id="slider-flip-m" data-role="slider">
+										<option value="on">Auto</option>
+										<option value="off" selected="">Manuel</option>
+									</select>
+								</div>
+							</div>
+						</form>
 					</div>
 					<div class="ui-grid-solo">					
 						<div id="graphique0"></div>
